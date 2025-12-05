f76fe5ac6482a3ff04cfc03dc023cf24e0e6123e
Yamin <yasminafitriazizah@gmail.com>
2025-12-05 11:07:42 +0700

    Update database port

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ $DB_HOST = "127.0.0.1";
 $DB_USER = "root";
 $DB_PASS = "";
 $DB_NAME = "rumahjamur";
-$DB_PORT = 3307;
+$DB_PORT = 3306;
 
 $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME, $DB_PORT);
 
@@ -15,3 +15,4 @@ if ($conn->connect_errno) {
     ");
 }
 ?>
+
